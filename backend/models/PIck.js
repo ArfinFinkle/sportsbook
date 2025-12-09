29893cf5078ad6a1922dc9bfe754ef225cf649af
nperl10 <75341725+nperl10@users.noreply.github.com>
2025-12-09 15:25:54 -0500

    Update PIck.js

--- backend/models/PIck.js
+++ backend/models/PIck.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const pickSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Pick", pickSchema);
